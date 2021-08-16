8161bd8f94ae9071721ab01f8eb3fa36134a6478
TheMulti0 <multi@codeprecise.com>
2021-08-16 15:21:08 +0300

    Updated tests

---
+++ Scraper.RabbitMq.Tests/MockScraperService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Scraper.Net;
+
+namespace Scraper.RabbitMq.Tests
+{
+    internal class MockScraperService : IScraperService
+    {
+        public Task<Author> GetAuthorAsync(string id, string platform, CancellationToken ct = new())
+        {
+            return Task.FromResult(new Author());
+        }
+
+        public async IAsyncEnumerable<Post> GetPostsAsync(string id, string platform, CancellationToken ct = new())
+        {
+            yield return new Post();
+        }
+    }
+}
