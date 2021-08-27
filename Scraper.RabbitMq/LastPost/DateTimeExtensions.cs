2df6c1dc5964a18234b72d4df0ef1adfd6d90b49
TheMulti0 <multi@codeprecise.com>
2021-08-27 13:06:36 +0300

    handle zero interval

--- Scraper.RabbitMq/LastPost/DateTimeExtensions.cs
+++ Scraper.RabbitMq/LastPost/DateTimeExtensions.cs
@@ -6,21 +6,9 @@ namespace Scraper.RabbitMq
     {
         public static DateTime Floor(this DateTime dateTime, TimeSpan interval)
         {
-            return dateTime.AddTicks(-(dateTime.Ticks % interval.Ticks));
-        }
-
-        public static DateTime Ceiling(this DateTime dateTime, TimeSpan interval)
-        {
-            var overflow = dateTime.Ticks % interval.Ticks;
-
-            return overflow == 0 ? dateTime : dateTime.AddTicks(interval.Ticks - overflow);
-        }
-
-        public static DateTime Round(this DateTime dateTime, TimeSpan interval)
-        {
-            var halfIntervalTicks = (interval.Ticks + 1) >> 1;
-
-            return dateTime.AddTicks(halfIntervalTicks - ((dateTime.Ticks + halfIntervalTicks) % interval.Ticks));
+            return interval == TimeSpan.Zero 
+                ? dateTime 
+                : dateTime.AddTicks(-(dateTime.Ticks % interval.Ticks));
         }
     }
 }
