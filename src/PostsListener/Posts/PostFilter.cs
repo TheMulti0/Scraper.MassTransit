171c0d5eb904aabfabc9c7051e069b88601a7a45
TheMulti0 <multi@codeprecise.com>
2021-10-17 17:55:11 +0300

    Added debug trace in post filter

--- src/PostsListener/Posts/PostFilter.cs
+++ src/PostsListener/Posts/PostFilter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Scraper.Net;
 
 namespace PostsListener
@@ -10,13 +11,16 @@ namespace PostsListener
         private const string Facebook = "facebook";
         private readonly LastPostFilter _lastPostFilter;
         private readonly PostUrlFilter _postUrlFilter;
+        private readonly ILogger<PostFilter> _logger;
 
         public PostFilter(
             LastPostFilter lastPostFilter,
-            PostUrlFilter postUrlFilter)
+            PostUrlFilter postUrlFilter,
+            ILogger<PostFilter> logger)
         {
             _lastPostFilter = lastPostFilter;
             _postUrlFilter = postUrlFilter;
+            _logger = logger;
         }
 
         public async Task<bool> FilterAsync(
@@ -24,13 +28,25 @@ namespace PostsListener
             string platform,
             CancellationToken ct)
         {
-            if (platform == Facebook)
+            async Task<bool> Filter()
             {
-                return await _lastPostFilter.FilterAsync(post, platform, TimeSpan.FromMinutes(1), ct) &&
-                       await _postUrlFilter.FilterAsync(post, ct);
+                if (platform == Facebook)
+                {
+                    return await _lastPostFilter.FilterAsync(post, platform, TimeSpan.FromMinutes(1), ct) &&
+                           await _postUrlFilter.FilterAsync(post, ct);
+                }
+
+                return await _lastPostFilter.FilterAsync(post, platform, TimeSpan.Zero, ct);
             }
 
-            return await _lastPostFilter.FilterAsync(post, platform, TimeSpan.Zero, ct);
+            if (await Filter())
+            {
+                _logger.LogDebug("Sending {}", post.Url);
+                return true;
+            }
+            
+            _logger.LogDebug("Not sending {}", post.Url);
+            return false;
         }
     }
 }
